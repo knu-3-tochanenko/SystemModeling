5268c91e990af9f279f9ad12c6c85b4e4b331496
Vladislav <itochanenko@gmail.com>
2019-09-11 15:20:55 +0300

    Added octave file

--- Lab 1/mail.py
+++ Lab 1/mail.py
@@ -1,2 +1,15 @@
 #%%
-print("Hello")
+import matplotlib.pyplot as plt
+
+#%%
+import numpy as np
+#%%
+with open('f14.txt') as file:
+    data = np.array([float(val) for val in file.read().split()])
+
+T = 5
+dt = 0.01
+time = np.arange(0, T + dt, dt)
+plt.grid(True)
+plt.plot(time, data)
+#%%
---
+++ Lab 1/octave.m
@@ -0,0 +1,6 @@
+dat = dlmread('f14.txt', ' ');
+T = 0:0.01:5.0;
+plot(T, dat);
+xlabel("T");
+ylabel("Data");
+title("Ai");

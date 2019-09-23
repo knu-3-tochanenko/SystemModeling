d835c1915161fecc911cf54913b5c8a0238cf1b9
Vladislav <itochanenko@gmail.com>
2019-09-23 14:58:10 +0300

    Completed first part of first lab

--- Lab 1/mail.py
+++ Lab 1/mail.py
@@ -1,15 +1,42 @@
-#%%
-import matplotlib.pyplot as plt
-
-#%%
-import numpy as np
-#%%
+# %%
+# Import libraries
+import numpy as numpy
+import matplotlib.pyplot as plot
+# %%
+# Open file
 with open('f14.txt') as file:
-    data = np.array([float(val) for val in file.read().split()])
-
+    inputData = numpy.array([float(val) for val in file.read().split()])
+# %%
+# Build det plot
 T = 5
 dt = 0.01
-time = np.arange(0, T + dt, dt)
-plt.grid(True)
-plt.plot(time, data)
-#%%
+time = numpy.arange(0, T+dt, dt)
+plot.grid(True)
+plot.rc('figure', figsize=(30.0, 10.0))
+plot.plot(time, inputData)
+# %%
+# Build time plot
+n = time.shape[0]
+frequency = numpy.zeros(n)
+for pointID in range(n):
+    sinFrequency = 0
+    cosFrequency = 0
+    for signal in range(n):
+        sinFrequency += inputData[signal] * \
+            numpy.sin(2.*numpy.pi*pointID*signal/float(n))
+        cosFrequency += inputData[signal] * \
+            numpy.cos(2.*numpy.pi*pointID*signal/float(n))
+    sinFrequency /= float(n)
+    cosFrequency /= float(n)
+    frequency[pointID] = numpy.sqrt(sinFrequency**2+cosFrequency**2)
+plot.grid(True)
+plot.plot(time, frequency)
+
+# %%
+# Find biggest value
+biggestValue = []
+for i in range(3, n // 2):
+    if numpy.max(frequency[i-3:i+3]) == frequency[i]:
+        biggestValue.append(i)
+        print(frequency[i])
+mainFrequency = biggestValue[0]/T
